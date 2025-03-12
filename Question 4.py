a9c96fad6a005cfe845a3809660477702de6dc45
Rajashree Sabale <114015610+RCS007@users.noreply.github.com>
2025-03-12 14:14:01 +0530

    Add files via upload

---
+++ Question 4.py
@@ -0,0 +1,12 @@
+# Question 4
+
+# Ask the user to type in any word and display it in upper case.
+
+# Ask the user to enter a word
+word = input("Type any word: ")
+
+# Convert the word to uppercase
+uppercase_word = word.upper()
+
+# Display the result
+print(f"The word in uppercase is: {uppercase_word}")
